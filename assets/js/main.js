84f5ac693b5af2ea20953e521a1094fdaed754c7
im20ki <inglyki2@gmail.com>
2026-01-28 16:40:26 +0900

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 window.addEventListener("load", () => {
-  const heroContent = document.querySelector(".hero-content");
-  heroContent.classList.add("is-active");
+    const heroContent = document.querySelector(".hero-content");
+    heroContent.classList.add("is-active");
 });
 
 const reveals = document.querySelectorAll(".reveal");
 
 const observer = new IntersectionObserver(
-  entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("is-visible");
-      }
-    });
-  },
-  { threshold: 0.2 }
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("is-visible");
+            }
+        });
+    },
+    { threshold: 0.2 }
 );
 
 reveals.forEach(el => observer.observe(el));

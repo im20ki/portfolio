881d3134bbf4d98b639ae43ac0f24857fb58c820
im20ki <inglyki2@gmail.com>
2026-01-28 11:52:02 +0900

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-console.log("JS 연결 성공");
+window.addEventListener("load", () => {
+  const heroContent = document.querySelector(".hero-content");
+  heroContent.classList.add("is-active");
+});
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+reveals.forEach(el => observer.observe(el));
